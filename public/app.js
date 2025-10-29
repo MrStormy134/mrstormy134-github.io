611dae0c957e7806365a16046a22fafe733467ff
MrStormy134 <zebracrossing185@outlook.com>
2025-10-29 11:05:30 +0000

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,11 @@
+const socket = io();
 
+let currentRoom = null;
+let meId = null;
+
+const nameInput = document.getElementById('name');
+const createBtn = document.getElementById('create');
+const joinBtn = document.getElementById('join');
+const joinCodeInput = document.getElementById('joinCode');
+const roomArea = document.getElementById('roomArea');
+const roomCodeEl = document.getElementB
